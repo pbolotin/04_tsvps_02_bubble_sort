0928e0548cde62a989236b76e7afcfa47857ecdc
pbolotin <pavel.bolotin@gmail.com>
2022-09-06 11:04:27 +0700

    Change the filenames are used in the program

--- 02_bubblesort.py
+++ 02_bubblesort.py
@@ -62,6 +62,30 @@ def selection_sort_enum_list_and_comment_steps(enum_list, order="a"):
         eprint()
             
     eprint("RESULT:", [t[0] for t in enum_list])
+    
+def bubble_sort_enum_list_and_comment_steps(enum_list, order="a"):
+    for step in range(1, len(enum_list)):
+        left_idx = step - 1
+        eprint(f"Step {step}: try to swap between rightest value in the left part and min(max) value in the right part")
+        eprint([t[0] for t in enum_list[:left_idx+1]],"|",[t[0] for t in enum_list[left_idx+1:]])
+        swap_candidate = left_idx
+        for right_idx in range(left_idx + 1, len(enum_list)):
+            if order == "a" and enum_list[swap_candidate][1] > enum_list[right_idx][1]:
+                eprint(f"SWAP CANDIDATE:{left_idx}:{enum_list[left_idx][0]},{right_idx}:{enum_list[right_idx][0]}")
+                swap_candidate = right_idx
+            elif order != "a" and enum_list[swap_candidate][1] < enum_list[right_idx][1]:
+                eprint(f"SWAP CANDIDATE:{left_idx}:{enum_list[left_idx][0]},{right_idx}:{enum_list[right_idx][0]}")
+                swap_candidate = right_idx
+                
+        if swap_candidate != left_idx:
+            eprint(f"SWAP {left_idx}:{enum_list[left_idx][0]}, {swap_candidate}:{enum_list[swap_candidate][0]}")
+            swap = enum_list[left_idx]
+            enum_list[left_idx] = enum_list[swap_candidate]
+            enum_list[swap_candidate] = swap
+            
+        eprint()
+            
+    eprint("RESULT:", [t[0] for t in enum_list])
 
 def output_result(enum_list):
     for n, s in enum_list:
@@ -73,8 +97,8 @@ def output_result(enum_list):
 
 def load_config_json():
     cfg = None
-    if check_file("01_selectsort_cfg.json"):
-        f = open("01_selectsort_cfg.json", "r")
+    if check_file("02_bubblesort_cfg.json"):
+        f = open("02_bubblesort_cfg.json", "r")
         cfg = json.load(f)
         f.close()
     else: #defaul config
@@ -82,7 +106,7 @@ def load_config_json():
     return cfg
     
 if __name__ == "__main__":
-    eprint("01_selectsort")
+    eprint("02_bubblesort")
     if not arguments_validation():
         print_help()
         exit()
